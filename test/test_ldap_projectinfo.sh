7b1ced239230d6616993d7827141516c3575d2a5
Kurt Lust <Kurt.Lust@uantwerpen.be>
2024-05-24 10:40:45 +0200

    Some improvements to the test scripts for userinfo and projectinfo.

--- test/test_ldap_projectinfo.sh
+++ test/test_ldap_projectinfo.sh
@@ -4,4 +4,8 @@ for name in $(/bin/ls -1 /var/lib/project_info/lust);
 do 
 #    echo $name 
     ../src/lumi-ldap-projectinfo.lua $name
+    if [[ $? -ne 0 ]]
+    then
+        echo -e "\nERROR for lumi-ldap-projectinfo.lua $name\n"
+    fi
 done
--- test/test_ldap_userinfo.sh
+++ test/test_ldap_userinfo.sh
@@ -4,4 +4,8 @@ for name in $(/bin/ls -1 /var/lib/user_info/lust);
 do 
 #    echo $name 
     ../src/lumi-ldap-userinfo.lua $name
+    if [[ $? -ne 0 ]]
+    then
+        echo -e "\nERROR for lumi-ldap-userinfo.lua $name\n"
+    fi
 done
